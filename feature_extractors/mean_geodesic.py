2224eeb4fc8dc46afa64ef48cf99997aa28101d7
Kansuke Ikehara <kansuke.ikehara@colorado.edu>
2016-01-01 16:13:20 -0700

    Bug fixed using np.nanmean

--- feature_extractors/mean_geodesic.py
+++ feature_extractors/mean_geodesic.py
@@ -1,21 +1,17 @@
 import numpy as np
 
-def mean_geodesic_distance(G,weighted=True):
+def mean_geodesic_distance(G):
     """
     input: python i-graph Graph object
     output: the mean geodesic distance (shortest path) of graph G
     """
-    if weighted:
-        weights = "weight" # an attribute for edge weight. You may change this depending on the gml file
-    else:
-        weights = None
 
     # a list of distances
     ds = []
     for i,v in enumerate(G.vs):
-        path_lengths = G.shortest_paths(v,weights=weights)[0]
+        path_lengths = G.shortest_paths(v,weights=None)[0]
         # since diagonal element is 0 (the distance of self loop is 0), delete the element
         del path_lengths[i]
         ds += path_lengths
 
-    return np.average(ds)
+    return np.nanmean(ds)
