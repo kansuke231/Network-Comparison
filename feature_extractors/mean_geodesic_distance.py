b9bdf4093e08384cf16d79a8867e33b6ca2ac7ca
Kansuke Ikehara <kansuke.ikehara@colorado.edu>
2016-05-01 10:16:31 -0600

    Implemented Random Sampling for mean geo dist.

--- feature_extractors/mean_geodesic_distance.py
+++ feature_extractors/mean_geodesic_distance.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 def mean_geodesic_distance(G):
     """
@@ -8,10 +9,22 @@ def mean_geodesic_distance(G):
 
     # a list of distances
     ds = []
-    for i,v in enumerate(G.vs):
-        path_lengths = G.shortest_paths(v,weights=None)[0]
-        # since diagonal element is 0 (the distance of self loop is 0), delete the element
-        del path_lengths[i]
-        ds += path_lengths
+    N = len(G.vs)
+    # if a network is small enough
+    if N < 10000:
+        for i,v in enumerate(G.vs):
+            path_lengths = G.shortest_paths(v,weights=None)[0]
+            # since diagonal element is 0 (the distance of self loop is 0), delete the element
+            del path_lengths[i]
+            ds.append(np.nanmean(path_lengths))
+    
+    # if it's really big
+    else:
+        for i,v in enumerate(random.sample(G.vs, int(N*0.01))):
+            path_lengths = G.shortest_paths(v,weights=None)[0]
+            # since diagonal element is 0 (the distance of self loop is 0), delete the element
+            del path_lengths[i]
+            ds.append(np.nanmean(path_lengths))
 
     return np.nanmean(ds)
+
