1d6e86eea395f467860f685d984e0ebed3430ccf
Kansuke Ikehara <kansuke.ikehara@colorado.edu>
2016-07-09 16:38:43 -0700

    Diamter sampling method for large graphs.

--- feature_extractors/diameter.py
+++ feature_extractors/diameter.py
@@ -1,2 +1,15 @@
+import random
+
 def diameter(G):
-	return G.diameter(directed=False)
+
+	N = len(G.vs)
+
+	if N < 10000:
+		return G.diameter(directed=False)
+		
+	else:
+		ds = []
+		for i,v in enumerate(random.sample(G.vs, int(N*0.01))):
+			path_lengths = G.shortest_paths(v,weights=None)[0]
+			ds+=path_lengths
+		return max(ds)
